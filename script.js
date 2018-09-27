7ac9bb1294fc5ce4f6e730910afea5466db5d137
Cassandra Matos <cassandra@matosmendez.com>
2018-09-27 08:57:03 -0700

    upload code base

---
+++ script.js
@@ -0,0 +1,19 @@
+// XMLHttpRequest is an API that can be used by JavaScript and other web browser scripting languages to transfer and manipulate XML data to and from a web server using HTTP, and establishing an independent connection channel between a webpage's Client-Side and Server-Side.
+
+// readyState --- Holds the status of the XMLHttpRequest
+// 0: request not initialized
+// 1: server connection established
+// 2: request received
+// 3: processing request
+// 4: request finished and response is ready
+
+// status --- Returns the status-number of a request
+// 200: "OK"
+// 403: "Forbidden"
+// 404: "Not Found"
+
+// onreadystatechange---Defines a function to be called when the readyState property changes
+
+
+
+
